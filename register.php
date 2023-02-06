636dfdfbdc2a997f3029a57c6f15bdb06a15d2de
SULPHALTEUR <82722560+SULPHALTEUR@users.noreply.github.com>
2023-02-06 12:29:55 +0100

    register

--- register.php
+++ register.php
@@ -18,7 +18,7 @@
                 $err_pseudo = "Ce champ ne peut pas être vide";
                 
             }else{
-                $req = $DB->prepare("SELECT * 
+                $req = $DB->prepare("SELECT id 
                 FROM users
                 WHERE pseudo = ?");
 
@@ -33,10 +33,10 @@
             }
 
             if(empty($email)){
-                $email = false;
+                $valid = false;
                 $err_email = "Ce champ ne peut pas être vide";
             }else{
-                $req = $DB->prepare("SELECT * 
+                $req = $DB->prepare("SELECT id 
                 FROM users
                 WHERE email = ?");
 
@@ -50,9 +50,25 @@
                 }
             }
 
+            if(empty($password)){
+                $valid = false;
+                $err_password = "Ce champ ne peut pas être vide";
+            }elseif($password <> $confpassword){
+                $valid = false;
+                $err_password = "Le mot de passe est different de la confirmation";
+            }
+
+
             //insertion a notre bdd
             if($valid){
-                echo 'ok';
+                $crypt_password = crypt($password, '$6$rounds=5000$ud|(&(66~O|t=+d8INm*jJSHk95Z7~YRPR.MGO D)0GJwTCH4x5(-PqFEH4');
+                $date_create = date('Y-m-d H:i:s');
+
+                $req = $DB->prepare("INSERT INTO users(pseudo, email, password, date_create, date_create) VALUES (?, ?, ?, ?, ?)");
+                $req->execute(array($pseudo, $email, $crypt_passwordd, $date_create));
+
+                header('Location: login.php');
+                exit;
             }else{
                 echo 'nok';
             }
@@ -78,8 +94,10 @@
 <?php if(isset($err_pseudo)){echo '<div>' . $err_pseudo . '</div>' ;} ?>
     <label>Pseudo</label>
     <input type="text" name="pseudo" value="<?php if(isset($pseudo)){echo $pseudo;}?>" placeholder="Pseudo">
+    <?php if(isset($err_email)){echo '<div>' . $err_email . '</div>' ;} ?>
     <label>Email</label>
     <input type="email" name="email" value="<?php if(isset($email)){echo $email;}?>" placeholder="Email">
+    <?php if(isset($err_password)){echo '<div>' . $err_password . '</div>' ;} ?>
     <label>Password</label>
     <input type="password" name="password" value="<?php if(isset($password)){echo $password;}?>" placeholder="Password">
     <label>confirm password</label>
